37354c63500ef25b580440cfec37846d94e470cb
Toms-B <toms.bergis06@gmail.com>
2023-11-24 14:53:40 +0200

    Izpildīju 3.uzd

---
+++ 3.uzdevums.py
@@ -0,0 +1,12 @@
+sk=0
+sk=int(input("Ievadi nepāra skaitli: "))
+
+para = int(sk//2)
+
+if para:
+    print("Tas nav nepāra skaitlis!")
+
+else:
+    print(f'Skaitlis {sk} ir nepāra skaitlis')
+
+  
